b351228cfcb28db40b8f34c0abb9a1151aae1f88
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-17 09:07:25 -0500

    suma 2 numeros

---
+++ main.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2, r;
+
+    cout << "Ingrese 1 número " << endl;
+    cin >>n1
+    cout<<"Ingrese 1 numero ";
+    cin >>n2
+    r=n1+n2
+    cout<<"\n la suma de n1+n2"<<" es:"<<r;
+    return 0;
+}
