a37c783f0b19b41efabe0dceb138f049fd131023
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-23 09:42:54 -0500

    Nuevecito

--- sumitas/main.cpp
+++ sumitas/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int primo,cont;
+    cont=0;
+    cout << "Ingrese un nro" << endl;
+    cin >> primo;
+	for(int verif=2; cont<=0 && primo>verif; verif=verif+1)
+        if(primo%verif==0)
+            cont=cont+1;
+    if (cont>0)
+        cout << "No es primo" << endl;
+    else
+        cout << "Es primo" << endl;
     return 0;
 }
