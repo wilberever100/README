44f778a307bcb7565de8a816d3d6c576b28bf576
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-10-12 09:58:31 -0500

    lasoa

---
+++ ejercicios clase/permanente2/para/include/Equipo.h
@@ -0,0 +1,15 @@
+#ifndef EQUIPO_H
+#define EQUIPO_H
+#include <string>
+
+class Equipo
+{
+    public:
+
+        Equipo();
+        Equipo(jugador1, jugador2, jugador3, jugador4, jugador5, jugador6, jugador7, jugador8,jugador9,jugador10,jugador11);
+
+
+};
+
+#endif // EQUIPO_H
---
+++ ejercicios clase/permanente2/para/src/Equipo.cpp
@@ -0,0 +1,22 @@
+#include "Equipo.h"
+
+Equipo::Equipo()
+{
+    jugador1="Gallese";
+    jugador2="Zambrano";
+    jugador3="El mudo";
+    jugador4="Ramos";
+    jugador5="Yotún";
+    jugador6="Cueva";
+    jugador7="Carrillo";
+    jugador8="Sergio Peña";
+    jugador9="Hurtado";
+    jugador10="Guerrero";
+    jugador11="Ruidíaz";
+    //ctor
+}
+
+Equipo::~Equipo()
+{
+    //dtor
+}
